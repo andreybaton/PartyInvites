21d148247b0f3ab90f7556b8a398de8108d93d63
andreybaton <andreybaton63@gmail.com>
2025-03-11 11:46:30 +0400

    финал

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -1,10 +1,16 @@
-﻿namespace PartyInvites.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required (ErrorMessage="please enter your name")]
         public string Name {  get; set; }
+        [Required(ErrorMessage = "please enter your email")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "please enter your phonee")]
         public string Phone { get; set; }
+        [Required(ErrorMessage = "please specify your decision")]
         public bool? WillAttend { get; set; }
     }
 }
